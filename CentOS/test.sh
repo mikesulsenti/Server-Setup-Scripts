7d27cc39656cf076155662cec5d86042b5adb00a
Drybones5 <michaelsulsenti@gmail.com>
2014-07-15 20:11:56 -0400

    CentOS added

---
+++ CentOS/test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo 'Going to setup, here we go...'
+echo '------------------------'
+
+read -p "Run setup $foo? [yn]" answer
+if [[ $answer = y ]] ; then
+  # echo <password> | sudo -S yum update ;;
+fi
