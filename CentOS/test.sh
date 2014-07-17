b8c46abe70ae4e02d56cdc7a6aea50f55944b2df
Drybones5 <michaelsulsenti@gmail.com>
2014-07-17 12:36:09 -0400

    no echo first line

--- CentOS/test.sh
+++ CentOS/test.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-echo 'Please Enter your password to access superuser'
 if [[ $UID -ne 0 ]]; then sudo "./test.sh"; exit 0; fi
-echo 'Now going to setup, here we go...'
+echo 'Now going to server setup, here we go...'
 echo '------------------------------------'
+
+read -p "Run the update script? [yn]" answer
+if [[ $answer = y ]] ; then
+  yum update ;
+fi
 
 read -p "Run the update script? [yn]" answer
 if [[ $answer = y ]] ; then
