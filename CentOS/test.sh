4ffdff33cdf8671124855c33673b3956f17fc4e1
Drybones5 <michaelsulsenti@gmail.com>
2014-07-17 12:01:54 -0400

    fixed

--- CentOS/test.sh
+++ CentOS/test.sh
@@ -3,7 +3,7 @@
 echo 'Going to setup, here we go...'
 echo '------------------------'
 
-read -p "Run setup $foo? [yn]" answer
+read -p "Run the update script? [yn]" answer
 if [[ $answer = y ]] ; then
-  # echo <password> | sudo -S yum update ;;
+  echo $user <password> | sudo -S yum update ;;
 fi
